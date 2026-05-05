d119a04fdf554813a770b5537694dc887e112a99
jonofrogg <JWELLS300@caledonian.ac.uk>
2026-05-05 07:08:44 +0100

    back to standard zoom
    first zoom commit wasn't fading when changing page (img override the z-axis)

--- zoomin.js
+++ zoomin.js
@@ -1,10 +1,5 @@
 const img = document.getElementById("zoomImg");
-const overlay = document.getElementById("overlay");
 
-function toggleZoom(){
+img.addEventListener("click", () => {
     img.classList.toggle("zoomed");
-    overlay.classList.toggle("active");
-}
-
-img.addEventListener("click", toggleZoom);
-overlay.addEventListener("click, toggleZoom");
+});
